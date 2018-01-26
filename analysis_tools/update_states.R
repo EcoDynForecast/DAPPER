1e15e00c38e36fdb45977275dd8c4c88b45c12c8
plaura1 <plaura1@vt.edu>
2018-01-26 17:21:55 -0500

    working EnKF for single time-step/location

---
+++ analysis_tools/update_states.R
@@ -0,0 +1,38 @@
+update_states <- function(mo,plotnum,output_dim,pars,site,nopars,nosite, thin_event, met){ # fix - need to alter to accept initial states as input 
+  #---CONTROL INFORMATION----------------------------
+  #rm(list = ls())
+  
+  tmp=.Fortran( "r3pg_interface",
+                output_dim=as.integer(output_dim),
+                met=as.double(met[plotnum,,mo]), 
+                pars=as.double(pars),
+                site = as.double(site),
+                thin_event = as.double(thin_event[plotnum,mo]),
+                out_var=as.double(array(0,dim=c(1,output_dim))),
+                nopars=as.integer(nopars),
+                nomet=as.integer(dim(met)[2]),
+                nosite = as.integer(nosite),
+                nooutputs=as.integer(output_dim),
+                nomonths_plot=as.integer(1),
+                nothin = 1,
+                exclude_hardwoods = as.integer(exclude_hardwoods[plotnum]),
+                mo_start_end = as.integer(c(1,1)),
+                nmonths = 1
+  )
+  
+  output=array(tmp$out_var, dim=c(1,output_dim))
+
+  
+  LAI  = output[4]
+  WS  = output[5]
+  WCR = output[6]
+  WFR = output[7]
+  Stem_Count = output[8]
+  ASW = output[14]
+  
+  DAPPERstates <- c(LAI = LAI, WS = WS, WCR = WCR, WFR = WFR, Stem_Count = Stem_Count, ASW =  ASW) # LAI, WS, WCR, WFR, Stem_Count, ASW
+  return(DAPPERstates)
+  
+}  
+
+  
